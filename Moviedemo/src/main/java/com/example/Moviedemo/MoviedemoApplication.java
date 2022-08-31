2c6bd955ef0379edf580443ab6cd039ccc4511cb
dabida <DanielBiman>
2022-08-31 21:09:14 +0200

    adding applicationV1

---
+++ Moviedemo/src/main/java/com/example/Moviedemo/MoviedemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Moviedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviedemoApplication.class, args);
+	}
+
+}
