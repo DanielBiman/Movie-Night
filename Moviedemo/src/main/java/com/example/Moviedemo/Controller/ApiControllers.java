cbcccdc5f4c4703187c3ee6b701406e893176c1e
dabida <DanielBiman>
2022-09-01 22:04:53 +0200

    Update

--- Moviedemo/src/main/java/com/example/Moviedemo/Controller/ApiControllers.java
+++ Moviedemo/src/main/java/com/example/Moviedemo/Controller/ApiControllers.java
@@ -2,6 +2,9 @@ package com.example.Moviedemo.Controller;
 
 import com.example.Moviedemo.Models.User;
 import com.example.Moviedemo.Repo.UserRepo;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
--- Moviedemo/src/main/java/com/example/Moviedemo/Repo/UserRepo.java
+++ Moviedemo/src/main/java/com/example/Moviedemo/Repo/UserRepo.java
@@ -2,5 +2,7 @@ package com.example.Moviedemo.Repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.Moviedemo.Models.User;
+
 public interface UserRepo extends JpaRepository<User, Long>{
 }
